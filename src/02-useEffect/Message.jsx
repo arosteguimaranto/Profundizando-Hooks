07a22026c9103edde45b61278b04bf14740e5bdd
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-02-22 13:14:01 -0600

    useEffect - Precauciones

--- src/02-useEffect/Message.jsx
+++ src/02-useEffect/Message.jsx
@@ -1,20 +1,34 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export const Message = () => {
 
-    useEffect(() => {
-      console.log('Message Mounted');
-    
-      return () => {
-        console.log('Message UnMounted');
-      }
-    }, [])
-    
+  const [coords, setCoords] = useState({x: 0, y: 0}) ;
+
+  useEffect(() => {
+
+    const onMouseMove = ({ x, y }) => {
+      //const coords = { x, y };
+      setCoords({x, y});
+    }
+    window.addEventListener('mousemove', onMouseMove);
+
+    return () => {
+
+      window.removeEventListener('mousemove', onMouseMove )
+
+    }
+
+
+  }, []);
+
+
+
 
   return (
     <>
-    <h3>Usuario ya existe</h3>
+      <h3>Usuario ya existe</h3>
+      {JSON.stringify(coords)}
     </>
-   
+
   )
 }
