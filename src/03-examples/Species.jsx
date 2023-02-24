2827d5b7d24ac4143e3942aca08c9394da06a86f
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-02-24 11:09:40 -0600

    useRef - Primer uso

---
+++ src/03-examples/Species.jsx
@@ -0,0 +1,12 @@
+
+
+export const Species = ({name, species}) => {
+
+    
+    return (
+        <blockquote className=" blockquote text-end">
+            <p className=" mb-1">{data.name}</p>
+            <footer className=" blockquote-footer">{data.species}</footer>
+        </blockquote>
+    )
+}
