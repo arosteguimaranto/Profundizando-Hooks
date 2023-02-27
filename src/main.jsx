a5fc3a2934d11e28a63d046723e63347c4e55793
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-02-27 13:01:42 -0600

    idea general de un reducer - Vía código

--- src/main.jsx
+++ src/main.jsx
@@ -11,12 +11,12 @@ import { Padre } from './07-tarea-memo/Padre'
 //import { CounterApp } from './01-useState/CounterApp'
 //import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 //import { SimpleForm } from './02-useEffect/SimpleForm'
-
+import './08-useReducer/intro-reducer';
 //import { HooksApp } from './HooksApp'
 import './index.css'
-
+/* 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Padre  />
   </React.StrictMode>,
-)
+) */
