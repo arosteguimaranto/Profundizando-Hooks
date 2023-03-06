ae587b0a29d10615e7e67c61fa4061def31ac0a7
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-03-06 11:21:28 -0600

    Preparación de nuestra aplicación con rutas

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
-import { SimpleForm } from './02-useEffect/SimpleForm';
+import {BrowserRouter} from 'react-router-dom'
+//import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
+//import { SimpleForm } from './02-useEffect/SimpleForm';
 //import { CallbackHook } from './06-memos/CallbackHook'
-import { Padre } from './07-tarea-memo/Padre'
+//import { Padre } from './07-tarea-memo/Padre'
 //import { MemoHook } from './06-memos/MemoHook'
 //import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 //import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
@@ -14,12 +15,20 @@ import { Padre } from './07-tarea-memo/Padre'
 //import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 //import { SimpleForm } from './02-useEffect/SimpleForm'
 //import './08-useReducer/intro-reducer';
-import { TodoApp } from './08-useReducer/TodoApp';
+//import { TodoApp } from './08-useReducer/TodoApp';
+import { MainApp } from './09-useContext/MainApp';
 //import { HooksApp } from './HooksApp'
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
- // <React.StrictMode>
-    <TodoApp  />
- // </React.StrictMode>,
+ <BrowserRouter>
+ 
+ {/* <React.StrictMode> */}
+ <MainApp  />
+{/* </React.StrictMode> */}
+
+ </BrowserRouter>
+ 
+ 
+
 )
