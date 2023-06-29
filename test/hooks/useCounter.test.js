c627c0cd7afa142a7c0ec016760cf9ca45a378d8
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-06-29 09:50:05 -0600

    Pruebas sobre useCounter - CustomHook

--- test/hooks/useCounter.test.js
+++ test/hooks/useCounter.test.js
@@ -7,5 +7,22 @@ describe("Pruebas en el useCounter", () => {
     const { counter, increment, decrement, reset } = result.current;
 
     expect(counter).toBe(10);
+    expect(decrement).toEqual(expect.any(Function));
+    expect(increment).toEqual(expect.any(Function));
+    expect(reset).toEqual(expect.any(Function));
+
   });
+
+  test('debe generar el counter con el valor de 100', () => {
+
+    const { result } = renderHook(() => useCounter(100));
+    const { counter } = result.current;
+    
+    expect(counter).toBe(100);
+
+  });
+
+
+
+
 });
