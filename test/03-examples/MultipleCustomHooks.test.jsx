2ea76f9048816cfdbc31e5c60189c139217b6727
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-07-11 16:37:57 -0600

    Evaluar respuesta del useFetch

--- test/03-examples/MultipleCustomHooks.test.jsx
+++ test/03-examples/MultipleCustomHooks.test.jsx
@@ -1,6 +1,6 @@
 const { render, fireEvent, screen } = require("@testing-library/react");
 const {
-  MultipleCustomHooks,
+    MultipleCustomHooks,
 } = require("../../src/03-examples/MultipleCustomHooks");
 
 import { useCounter } from "../../src/hooks/useCounter";
@@ -10,62 +10,67 @@ jest.mock("../../src/hooks/useFetch");
 jest.mock("../../src/hooks/useCounter");
 
 describe("Pruebas en el <MultipleCustomHooks/>", () => {
-  const mockIncrement = jest.fn();
+    const mockIncrement = jest.fn();
 
-  useCounter.mockReturnValue({
-    counter: 1,
-    increment: mockIncrement,
-  });
-
-  beforeEach(() => {
-    jest.clearAllMocks();
-  });
-
-  test("debe de mostrar el componente por defecto", () => {
-    useFetch.mockReturnValue({
-      data: null,
-      isLoading: true,
-      hasError: null,
+    useCounter.mockReturnValue({
+        counter: 1,
+        increment: mockIncrement,
     });
 
-    render(<MultipleCustomHooks />);
-
-    //aserciones sabiendo el contenido
-    expect(screen.getByText("Loading..."));
-    expect(screen.getByText("Rick And Morty"));
-
-    //Tomar un boton o un elemento
-
-    const nextButton = screen.getByRole("button", { name: "Next Character" });
-    expect(nextButton.disabled).toBeTruthy();
-  });
-
-/*   test("debe de mostrar un personaje ", () => {
-    useFetch.mockReturnValue({
-      data: [{ name: "Dylan", species: "Human" }],
-      isLoading: false,
-      hasError: null,
+    beforeEach(() => {
+        jest.clearAllMocks();
     });
 
-    render(<MultipleCustomHooks />);
-    expect(screen.getByText("Dylan").toBeTruthy());
-    expect(screen.getByText("Human").toBeTruthy());
+    test("debe de mostrar el componente por defecto", () => {
+        useFetch.mockReturnValue({
+            data: null,
+            isLoading: true,
+            hasError: null,
+        });
 
-    const nextButton = screen.getByRole("button", { name: "Next Character" });
-    expect(nextButton.disabled).toBeFalsy();
-  }); */
+        render(<MultipleCustomHooks />);
 
-  test("debe de llamar la funcion de incrementar", () => {
-    useFetch.mockReturnValue({
-      data: [{ name: "Dylan", specie: "Human" }],
-      isLoading: false,
-      hasError: null,
+        //aserciones sabiendo el contenido
+        expect(screen.getByText("Loading..."));
+        expect(screen.getByText("Rick And Morty"));
+
+        //Tomar un boton o un elemento
+
+        const nextButton = screen.getByRole("button", { name: "Next Character" });
+        expect(nextButton.disabled).toBeTruthy();
     });
 
-    render(<MultipleCustomHooks />);
-    const nextButton = screen.getByRole("button", { name: "Next Character" });
-    fireEvent.click(nextButton);
+    /*   test("debe de mostrar un personaje ", () => {
+        useFetch.mockReturnValue({
+          data: [{ name: "Dylan", species: "Human" }],
+          isLoading: false,
+          hasError: null,
+        });
+    
+        render(<MultipleCustomHooks />);
+        expect(screen.getByText("Dylan").toBeTruthy());
+        expect(screen.getByText("Human").toBeTruthy()); 
+    
+        const nextButton = screen.getByRole("button", { name: "Next Character" });
+        expect(nextButton.disabled).toBeFalsy();
+      }); */
 
-    expect(mockIncrement).toHaveBeenCalled();
-  });
+    test("debe de llamar la funcion de incrementar", () => {
+
+    
+        useFetch.mockReturnValue({
+            data: [{ name: "Dylan", specie: "Human" }],
+            isLoading: false,
+            hasError: null,
+        });
+
+
+
+
+        render(<MultipleCustomHooks />);
+        const nextButton = screen.getByRole("button", { name: "Next Character" });
+        fireEvent.click(nextButton);
+
+        expect(mockIncrement).toHaveBeenCalled();
+    });
 });
